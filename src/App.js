8a2cd73674fd4d5594626787bdf79a93a610b09f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-24 05:08:15 +0600

    Tailwind css setup copleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h2>Hello Medi Tools</h2>
+      <h2 className='text-3xl text-green-500'>Hello Medi Tools</h2>
     </div>
   );
 }
