a3b7a983ac92b7851fab72c4cd8e9230a05738b2
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-24 05:14:52 +0600

    Installed React Toastify for notification

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
       <h2 className='text-3xl text-green-500'>Hello Medi Tools</h2>
       <button class="btn btn-primary">Button</button>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
