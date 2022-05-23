6904fd4083ecd03777dc2b9796def5e6a0dcc7b6
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-24 05:46:15 +0600

    Initial Folder Structure created

---
+++ src/Pages/Authentication/RequireAdmin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RequireAdmin = () => {
+    return (
+        <div>
+            <h2>Require Admin</h2>
+        </div>
+    );
+};
+
+export default RequireAdmin;
