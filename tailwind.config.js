cfc641c5120b9c4e83a43a805d34ad5e878c65d2
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-24 05:34:32 +0600

    setup primary color and secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     themes: [
       {
         doctortheme: {
-          primary: "#0FCFEC",
-          secondary: "#19D3AE",
+          primary: "#fc575e",
+          secondary: "#f7b42c",
           accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
