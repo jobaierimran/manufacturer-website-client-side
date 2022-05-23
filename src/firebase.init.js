5e0b2671b2b715c99042b7ad94bb4f05b6c77f9f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-24 05:25:00 +0600

    firebse config updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -12,16 +12,6 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-
-
-
-
-    /*     apiKey: "AIzaSyA6cGrZSio_GmKnErgxSUF7hM2U2ZonxNw",
-        authDomain: "medi-tools-manufacture.firebaseapp.com",
-        projectId: "medi-tools-manufacture",
-        storageBucket: "medi-tools-manufacture.appspot.com",
-        messagingSenderId: "598027404807",
-        appId: "1:598027404807:web:02c6bf9ede46e8d1c88ca2" */
 };
 
 // Initialize Firebase
