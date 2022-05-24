3964d5676e667c9f66083370b5772818baf92f11
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-24 06:42:15 +0600

    Loading Spinner added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -17,9 +17,9 @@ const Banner = () => {
                 </div>
             </div>
             <div className="flex justify-center w-full py-2 gap-2">
-                <a href="#item1" className="btn btn-xs">1</a>
-                <a href="#item2" className="btn btn-xs">2</a>
-                <a href="#item3" className="btn btn-xs">3</a>
+                <a href="#item1" className="btn btn-primary btn-xs">1</a>
+                <a href="#item2" className="btn btn-primary btn-xs">2</a>
+                <a href="#item3" className="btn btn-primary btn-xs">3</a>
             </div>
         </div>
     );
