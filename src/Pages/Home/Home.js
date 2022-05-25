51ede022dd40bfa4fdd41f31730ad562fefcde24
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-25 06:15:34 +0600

    Our Business summary is completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Loading from '../Shared/Loading';
 import Banner from './Banner';
+import Summary from './Summary';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Summary></Summary>
         </div>
     );
 };
