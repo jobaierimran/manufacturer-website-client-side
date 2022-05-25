f92c3af791d641866a1b76df12191ff0da7a9d2e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-25 06:58:50 +0600

    Class Name updated

--- src/Pages/Home/Explore.js
+++ src/Pages/Home/Explore.js
@@ -4,18 +4,18 @@ const Explore = () => {
     return (
         <div className='my-12'>
             <h2 className='text-primary text-3xl text-center font-bold mb-8'>Explore Us</h2>
-            <div class="hero py-8" style={{
+            <div className="hero py-8" style={{
                 background: `url(${banner1})`,
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
                 boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.6)"
             }}>
-                <div class="hero-overlay bg-opacity-60"></div>
-                <div class="hero-content text-center text-neutral-content">
-                    <div class="max-w-md">
-                        <h1 class="mb-5 text-5xl font-bold">Hello Peoples</h1>
-                        <p class="mb-5">We are a renowned Manufacturer and Exporter of the quality approved assortment of Medical Equipment. These products are admired for their excellent resistance...</p>
-                        <button class="btn btn-primary">Explore More</button>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello Peoples</h1>
+                        <p className="mb-5">We are a renowned Manufacturer and Exporter of the quality approved assortment of Medical Equipment. These products are admired for their excellent resistance...</p>
+                        <button className="btn btn-primary">Explore More</button>
                     </div>
                 </div>
             </div>
