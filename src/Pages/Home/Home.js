f001b3c2c686f12388884ea8424e2ecc2e491a04
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-25 07:04:28 +0600

    Contact section completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Loading from '../Shared/Loading';
 import Banner from './Banner';
+import Contact from './Contact';
 import Explore from './Explore';
 import Reviews from './Reviews';
 import Summary from './Summary';
@@ -12,6 +13,7 @@ const Home = () => {
             <Summary></Summary>
             <Reviews></Reviews>
             <Explore></Explore>
+            <Contact></Contact>
         </div>
     );
 };
