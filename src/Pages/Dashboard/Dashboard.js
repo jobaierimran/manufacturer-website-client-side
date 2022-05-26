abbd8805afd117cb6e11cf2c4902bca614bc2d4b
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-26 23:20:48 +0600

    show user route for user and adming route for admin

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,10 +3,12 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../firebase.init';
 import useAdmin from '../../hooks/useAdmin';
+import Loading from '../Shared/Loading';
 
 const Dashboard = () => {
     const [user] = useAuthState(auth)
     const [admin] = useAdmin(user);
+
     return (
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
@@ -19,14 +21,15 @@ const Dashboard = () => {
                 <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to="/dashboard/myOrders">My Orders</Link></li>
-                    <li><Link to="/dashboard/addReview">Add Review</Link></li>
-
-                    {admin &&
+                    {admin ?
                         <>
                             <li><Link to="/dashboard/addProduct">Add Product</Link></li>
                             <li><Link to="/dashboard/users">All Users</Link></li>
                             <li><Link to="/dashboard/manageProducts">Manage Products</Link></li>
+                        </> :
+                        <>
+                            <li><Link to="/dashboard/myOrders">My Orders</Link></li>
+                            <li><Link to="/dashboard/addReview">Add Review</Link></li>
                         </>
                     }
                 </ul>
